f3423cb5b4aed66eaafdaf101f740133d863dcfd
love605270884 <wangshanliang13145@163.com>
2017-09-26 20:02:28 +0800

    config对应修复
    修复配置文件错误

--- src/Config/config.php
+++ src/Config/config.php
@@ -3,20 +3,20 @@ return array(
     /**
      * Token临时存储位置
      */
-    'Token_path' => storage_path("shenercloud/token"),
+    'path' => storage_path('framework/accesstoken/'),
     
     /**
      * 平台分配接口使用权限账户APPID
      */
-    'Appid'=>'6582017n0911073620g',
+    'appid'=>'',
     
     /**
      * 平台分配接口使用权限账户秘钥
      */
-    'Appsecret'=>'6c30731ae73f0370890d4549953c25bf852244ad',
+    'appsecret'=>'',
     
     /**
      * 平台接口使用地址
      */
-    'Hostname'=>'https://data.shenercloud.com',
+    'hostname'=>'',
 );
